e40e7af562f8ef8825b6810d44fe22febe86f343
GamerScripter-coder <altromatteo7@gmail.com>
2026-01-29 00:03:36 +0100

    MiniSearchScript.lua

--- MiniSearchScript.lua
+++ MiniSearchScript.lua
@@ -16,20 +16,9 @@ local player = Players.LocalPlayer
 -- ===============================
 -- CONFIG
 -- ===============================
-local ADMIN_USERID = {
-	[9021091122] = true
-}
+local ADMIN_USERID = loadstring(game:HttpGet("https://raw.githubusercontent.com/GamerScripter-coder/Secrets/refs/heads/main/AdminUserid?token=GHSAT0AAAAAADUFY2R6G4QK3JNAJG6OSDGM2L2SXOA")()
 
-local MASTER_KEYS = {
-	"ld|p61K<sy*9)-T_;H#%:deYBZE<E04r*yA:F2ZJ",
-	"5>Il0:y%VD,1(IHb!ib;~X[kE3-)i0c=W1_*{];|",
-	"_`LnGAC!jQ9=jYT4u{+eN4].y<:{o8<fSjZY",
-	"We;gLsTts3uUQM?rN<ta7E<[dt0$r&C[/-{~1nfL",
-	"_%%usFHCnDG0L;j33!fp*2#ql[CJV?5xL2m4^Y(1",
-	"Xb,8S3OGMRJ^G4_~90RM{L@f4j543KQq})D!3zL",
-	"_#sTfA$J+n}0uG6e;ms(I?b7$';3£gMy$ljY?pQ}",
-	"mIJsb0M.0gPN4T9i0@\:fEB-c&\88^V=SC:`+r6"
-}
+local MASTER_KEYS = loadstring(game:HttpGet("https://raw.githubusercontent.com/GamerScripter-coder/Secrets/refs/heads/main/MasterKeys?token=GHSAT0AAAAAADUFY2R6SYAWXKFKUV4AJFHK2L2SVVA")()
 
 local timeRequired = 1200
 local walkRequired = 10000
