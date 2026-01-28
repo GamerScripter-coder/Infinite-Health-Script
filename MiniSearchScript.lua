497708daf3cfd87f4393acb150b16018f534c635
GamerScripter-coder <altromatteo7@gmail.com>
2026-01-28 19:10:28 +0100

    MiniSearchScript.lua

--- MiniSearchScript.lua
+++ MiniSearchScript.lua
@@ -16,10 +16,20 @@ local player = Players.LocalPlayer
 -- ===============================
 -- CONFIG
 -- ===============================
-local ADMIN_USERID = 9021091122
+local ADMIN_USERID = {
+	[9021091122] = true,
+	[8629145749] = true
+}
 
 local MASTER_KEYS = {
-	"ld|p61K<sy*9)-T_;H#%:deYBZE<E04r*yA:F2ZJ"
+	"ld|p61K<sy*9)-T_;H#%:deYBZE<E04r*yA:F2ZJ",
+	"5>Il0:y%VD,1(IHb!ib;~X[kE3-)i0c=W1_*{];|",
+	"_`LnGAC!jQ9=jYT4u{+eN4].y<:{o8<fSjZY",
+	"We;gLsTts3uUQM?rN<ta7E<[dt0$r&C[/-{~1nfL",
+	"_%%usFHCnDG0L;j33!fp*2#ql[CJV?5xL2m4^Y(1",
+	"Xb,8S3OGMRJ^G4_~90RM{L@f4j543KQq})D!3zL",
+	"_#sTfA$J+n}0uG6e;ms(I?b7$';3£gMy$ljY?pQ}",
+	"mIJsb0M.0gPN4T9i0@\:fEB-c&\88^V=SC:`+r6"
 }
 
 local timeRequired = 1200
@@ -156,7 +166,7 @@ Instance.new("UICorner", keyBox)
 -- ===============================
 -- ADMIN KEY UI (SOLO TU)
 -- ===============================
-if player.UserId == ADMIN_USERID then
+if ADMIN_USERID[player.UserId] then
 	local adminFrame = Instance.new("Frame")
 	adminFrame.Size = UDim2.new(0.9,0,0.22,0)
 	adminFrame.Position = UDim2.new(0.05,0,0.5,0)
@@ -240,13 +250,6 @@ end)
 task.spawn(function()
 	while not keyValid do
 		task.wait(1)
-		
-		-- Controllo MASTER KEY
-		for _, k in ipairs(MASTER_KEYS) do
-			if keyBox.Text == k then
-				keyValid = true
-			end
-		end
 
 		-- Aggiorna label azioni
 		if not keyValid then
@@ -267,6 +270,12 @@ end)
 -- BOTTONE VERIFICA
 -- ===============================
 verifyBtn.MouseButton1Click:Connect(function()
+    for _, k in ipairs(MASTER_KEYS) do
+			if keyBox.Text == k then
+				keyValid = true
+			end
+		end
+	
 	if keyValid then
 		keyFrame:Destroy()
 		scrollFrame.Visible = true
