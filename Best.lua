33ea8571b222749101838a136a0b780ee63f42f7
GamerScripter-coder <altromatteo7@gmail.com>
2026-03-13 16:47:06 +0100

    Best.lua

--- Best.lua
+++ Best.lua
@@ -259,7 +259,7 @@ local script = G2L["b"];
 	-- Velocità diverse per ogni esperienza
 	local PlaceSpeed = {
 		[131623223084840] = 250, -- esempio esperienza 1
-		[109983668079237] = 25   -- esempio esperienza 2
+		[109983668079237] = 35   -- esempio esperienza 2
 	}
 	
 	local function GetSpeed()
