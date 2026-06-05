6b7a879529adb0d98de2710d8fad767b8dfd76f0
GamerScripter-coder <altromatteo7@gmail.com>
2026-06-05 23:15:18 +0200

    Rayfield.lua

---
+++ Rayfield.lua
@@ -0,0 +1,97 @@
+local RayfieldModule = {}
+local RM = RayfieldModule = {}  
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local function CreateWindowAndReturn(NameWindow, WindowIcon, WLoadingTitle, WLoadingSubTitle)
+	return Rayfield:CreateWindow({
+		Name = NameWindow.."(CheaterHub)",
+		Icon = WindowIcon,
+		LoadingTitle = WLoadingTitle.."(CheaterHub)",
+		LoadingSubtitle = WLoadingSubTitle.."(CheaterHub)",
+		ConfigurationSaving = {
+			Enabled = true,
+			FolderName = nil,
+			FileName = "CheaterHub"
+		}
+	})
+end
+
+local function CreateTabAndReturn(Window, Name, Img)
+	return Window:CreateTab(Name.."(CheaterHub)", Img)
+end
+
+local function CreateButton(Tab, BtnName, Clickfunc)
+	return Tab:CreateButton({
+		Name = BtnName.."(CheaterHub)",
+		Callback = function()
+			Clickfunc()
+		end
+	})
+end
+
+local function CreateToggle(Tab, TogName, TogFlag, Changefunc)
+	return Tab:CreateToggle({
+		Name = TogName.."(CheaterHub)",
+		CurrentValue = false,
+		Flag = TogFlag,
+		Callback = function(v)
+			Changefunc(v)
+		end
+	})
+end
+
+local function CreateSlider(Tab, SlidName, SlidFlag, CurrentV, Inc, SlidRange, Changefunc)
+	return Tab:CreateSlider({
+		Name = SlidName.."(CheaterHub)",
+		Range = SlidRange,
+		Increment = Inc,
+		CurrentValue = CurrentV,
+		Flag = SlidFlag,
+		Callback = function(v)
+			Changefunc(v)
+		end
+	})
+end
+
+local function CreateInput(Tab, InpName, InpFlag, InpHolder, Changefunc)
+	return Tab:CreateInput({
+		Name = InpName.."(CheaterHub)",
+		CurrentValue = "",
+		PlaceholderText = InpHolder,
+		RemoveTextAfterFocusLost = false,
+		Flag = InpFlag,
+		Callback = function(t)
+			Changefunc(t)
+		end
+	})
+end
+
+local function CreateLabel(Tab, Name, Icon)
+   Tab:CreateLabel(Name.."(CheaterHub)", Icon, Color3.fromRGB(255,255,255), false)
+end
+
+function RM:CWAR(NameWindow, WindowIcon, WLoadingTitle, WLoadingSubTitle)
+  return CreateWindowAndReturn(NameWindow, WindowIcon, WLoadingTitle, WLoadingSubTitle)
+end
+
+function RM:CTAR(Window, Name, Img)
+  return CreateTabAndReturn(Window, Name, Img)
+end
+
+function RM:CB(Tab, BtnName, Clickfunc)
+  CreateButton(Tab, BtnName, Clickfunc)
+end
+
+function RM:CT(Tab, TogName, TogFlag, Changefunc)
+  CreateToggle(Tab, TogName, TogFlag, Changefunc)
+end
+
+function RM:CS(Tab, SlidName, SlidFlag, CurrentV, Inc, SlidRange, Changefunc)
+  CreateSlider(Tab, SlidName, SlidFlag, CurrentV, Inc, SlidRange, Changefunc)
+end
+
+function RM:CI(Tab, InpName, InpFlag, InpHolder, Changefunc)
+  CreateInput(Tab, InpName, InpFlag, InpHolder, Changefunc)
+end
+
+return RM
